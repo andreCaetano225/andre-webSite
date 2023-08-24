8bf9f037dd7caedb7853c998291e31cd98d74363
andre caetano <andre.caetano2007@hotmail.com>
2023-08-23 22:48:43 -0300

    Theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from './theme-provider'
-import { ThemeSwitcher } from '@/components/themeSwitcher'
+import { ThemeSwitcher } from '../components/ThemeSwitcher'
 
 const inter = Inter({ subsets: ['latin'] })
 
