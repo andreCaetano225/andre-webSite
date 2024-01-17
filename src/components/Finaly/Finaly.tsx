bc07368baa4c864a5765dc1430145473f51ca1c7
andre caetano <andre.caetano2007@hotmail.com>
2024-01-16 22:00:49 -0300

    atualizando data forever

--- src/components/Finaly/Finaly.tsx
+++ src/components/Finaly/Finaly.tsx
@@ -5,7 +5,7 @@ export const Finaly = () => {
     return (
         <>
             <div className="flex items-center justify-center  sm:p-24 md:p-24 lg:p-24 xl:p-24 text-[11px] text-lightTitle dark:text-darkTitle">
-                <p>© 2023. construído com cuidado por André Caetano</p>
+                <p>© 2024. construído com cuidado por André Caetano</p>
             </div>
         </>
     )
