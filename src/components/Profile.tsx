a4ea245913786bbec67c36d473c5e145c0b10779
André de Sousa <andre.caetano2007@hotmail.com>
2024-05-22 21:53:13 -0300

    atualizando hearder e texto experiencia

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -19,7 +19,7 @@ export const Profile = () => {
                 <div >
                     <p className="font-bold  text-lightTitle text-sm sm:text-sm md:text-3xl lg:text-3xl xl:text-3xl dark:text-darkTitle">
                         Olá 👋, <br /> Eu sou o <br /> <span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-lightGrandBlue via-lightGrandPink to-[#7D5FCF]">André Caetano de Sousa</span> <br />
-                        E sou desenvolvedor full stack
+                        E sou desenvolvedor front-end
                     </p>
                 </div>
                 <div className="rounded-[992px]  bg-gradient-to-r from-lightGrandBlue via-lightGrandPink to-[#7D5FCF] p-1">
