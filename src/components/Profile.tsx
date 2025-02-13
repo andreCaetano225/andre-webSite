208efcb6f10d8342ea07dab02ea9696668bf2b38
André de Sousa <andre.caetano2007@hotmail.com>
2025-02-13 16:10:40 -0300

    iniciando atualização 2025

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -18,20 +18,21 @@ export const Profile = () => {
     return (
         <>
             <div className="flex flex-row items-center justify-between " data-aos="fade-up">
+            <div className="rounded-[992px]  bg-gradient-to-r from-purple-700 via-indigo-500 to-purple-700 p-1">
+                <Image
+                    alt="André Caetano"
+                    src={ProfileImage}
+                    className="rounded-full w-[100px] sm:w-[100px] md:w-[300px] lg:w-[300px] xl:w-[300px]"
+                />
+            </div>
                 <div >
                     <p className="font-bold  text-lightTitle text-sm sm:text-sm md:text-3xl lg:text-3xl xl:text-3xl dark:text-darkTitle">
-                        Olá 👋, <br /> Eu sou o <br /> <span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-lightGrandBlue via-lightGrandPink to-[#7D5FCF]">André Caetano de Sousa</span> <br />
+                        Olá 👋, <br /> Eu sou o <br /> <span className="font-extrabold text-transparent  bg-clip-text bg-gradient-to-r from-purple-700 via-indigo-500 to-purple-700">André Caetano de Sousa</span> <br />
                         E sou desenvolvedor front-end
-                    </p>
+                       
+                    </p> 
                 </div>
-                <div className="rounded-[992px]  bg-gradient-to-r from-lightGrandBlue via-lightGrandPink to-[#7D5FCF] p-1">
-                    <Image
-                        alt="André Caetano"
-                        src={ProfileImage}
-                        className="rounded-full w-[100px] sm:w-[100px] md:w-[300px] lg:w-[300px] xl:w-[300px]"
-                    />
-
-                </div>
+             
             </div>
 
 
